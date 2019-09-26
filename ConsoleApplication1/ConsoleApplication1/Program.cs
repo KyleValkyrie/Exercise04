ce0dd4815fe5455062f53caca8265ab82ccf53c1
KyleValkyrie <baolegend9@gmai.com>
2019-09-26 19:56:10 +0700

    Excerise04

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Hello Word!");
+        }
+        public static bool IsOdd(int n)
+        {
+            return true;
+        }
+        public static bool IsEven(int n)
+        {
+            return true;
         }
     }
 }
